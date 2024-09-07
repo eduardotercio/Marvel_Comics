84e359a4100600936f5c1472b2be8d28b24821ab
Eduardo Tercio <dudubezerra33@hotmail.com>
2024-09-07 14:35:56 -0300

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HQs Marvel"
+include(":app")
+include(":common")
+include(":feature")
+include(":designsystem")
+include(":feature:home")
+include(":feature:favorites")
+include(":feature:splash")
+include(":feature:comic")
